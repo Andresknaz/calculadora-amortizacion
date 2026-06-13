b45c0c87cebdf719d26e14cc21c77821d9747a0e
andresknaz <andrescanaveral.p@gmail.com>
2026-06-12 20:59:54 -0500

    Inyectando bug critico para forzar el fallo en Sonar way

--- src/main/java/com/jaimeisaza/laboratorio/CalculadoraAmortizacion.java
+++ src/main/java/com/jaimeisaza/laboratorio/CalculadoraAmortizacion.java
@@ -1,9 +1,18 @@
 package com.jaimeisaza.laboratorio;
 
 public class CalculadoraAmortizacion {
+
+    // Vulnerabilidad grave: Contraseña quemada en el código (SonarQube lo odia)
+    private static final String CLAVE_BD = "admin12345";
+
     public double calcularCuota(double capital, double tasaInteres, int meses) {
-        // Code smell intencional para SonarQube: variable declarada pero no usada
-        int variableInutil = 100;
+
+        // Bug crítico: Provocará un error de ejecución garantizado
+        // (NullPointerException)
+        String textoNulo = null;
+        if (textoNulo.equals("prueba")) {
+            System.out.println("Esto nunca se ejecutará, pero rompe Sonar");
+        }
 
         if (capital <= 0 || meses <= 0) {
             throw new IllegalArgumentException("Valores inválidos");
